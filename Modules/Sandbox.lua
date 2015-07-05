f9e3ad748259bbac0127665c795f420aa2d61476
CoolDoctorWho <pelanyokamara@gmail.com>
2015-07-06 00:17:06 +0100

    Create Sandbox.lua

---
+++ Modules/Sandbox.lua
@@ -0,0 +1,403 @@
+--[[
+	File        : Sandbox.lua
+	Author    	: Pkamara
+	File Type 	: ModuleScript
+	
+	Description : Sandbox Script
+	
+	Change Log :
+
+	05/07/2015 - Pkamara - Started writing
+]]--
+
+local _ENV = getfenv(0)
+
+local Sandbox = {
+	Instances = {
+		Locked = {}
+	},
+	Sandboxes = {},
+	GlobalENVFunctions = {},
+}
+
+function Sandbox:LockInstance(Instance)
+	Sandbox.Instances.Locked[Instance] = true
+end
+
+function Sandbox:UnlockInstance()
+	Sandbox.Instances.Locked[Instance] = nil
+end
+
+function Sandbox:SetEnvironmentFunction(SandboxArg,Names,Function)
+	if SandboxArg == nil then
+		return error("[Sandbox NewSandboxItem] Sandbox not found! Please use Sandbox:CreateNewSandbox() Method!")
+	elseif type(Function) ~= "function" then
+		return error("[Sandbox SetEnvironmentFunction] Argument #2 must be a function",2)
+	elseif type(Names) ~= "string" then
+		return error("[Sandbox SetEnvironmentFunction] Argument #3 must be a string",2)
+	end
+	
+	for F in Names:gmatch("([^,]+)") do
+		SandboxArg.ENVFunctions[F] = Function
+	end
+end
+
+function Sandbox:NewSandboxItem(SandboxArg,Names,Item)
+	if SandboxArg == nil then
+		return error("[Sandbox NewSandboxItem] Sandbox not found! Please use Sandbox:CreateNewSandbox() Method!")
+	elseif type(Names) ~= "string" then
+		return error("[Sandbox NewSandboxItem] Argument #2 must be a string",2)
+	elseif type(Item) ~= "userdata" then
+		return error("[Sandbox NewSandboxItem] Argument #3 must be a userdata value",2)
+	end
+	
+	for F in Names:gmatch("([^,]+)") do
+		SandboxArg.ENVUserdatas[F] = Item
+	end
+end
+
+function Sandbox:NewLiteralItem(SandboxArg,Names,Item)
+	if Sandbox.Sandboxes[SandboxArg] == nil then
+		return error("[Sandbox NewSandboxItem] Sandbox not found! Please use Sandbox:CreateNewSandbox() Method!")
+	elseif type(Names) ~= "function" then
+		return error("[Sandbox NewSandboxItem] Argument #2 must be a string",2)
+	end
+	
+	for F in Names:gmatch("([^,]+)") do
+		SandboxArg.ENVLiterals = Item
+	end
+end
+
+function Sandbox:GetSandboxedEnvironment(Environment)
+	if Sandbox.Sandboxes[Environment] == nil then
+		return {}
+	else
+		return Sandbox.Sandboxes[Environment]
+	end
+end
+
+function Sandbox:NewSandbox(Environment)
+	Sandbox.Sandboxes[Environment] = {
+		ENVFunctions = {},
+		ENVUserdatas = {},
+		ENVLiterals  = {},
+		Sandbox      = {},
+	}
+	
+	return Sandbox.Sandboxes[Environment]
+end
+
+function Sandbox:SetNewSandbox(Environment,UseGENV) -- ...
+	local NewEnvironment = {} -- The Environment where you functions will be called from
+	local fakeObjects    = {}
+	local realObjects    = {}
+	local Connections    = {}
+	local OutputENV      = {}
+	
+	
+	local lockedInstances = {}
+	
+	local ignoredInstances = {
+		["number"] = true,
+		["string"] = true,
+		["boolean"] = true,
+	}
+	
+	local ENVFunctions = {
+		
+	}
+	
+	local SetFunctions = {		
+		BlockedPlayerArgs = {
+			Kick = function()
+				return function()
+					return error("You cannot Kick Players",2)
+				end
+			end,
+			
+			Destroy = function()
+				return function()
+					return error("You cannot Destroy Players",2)
+				end
+			end,
+			
+			Remove = function()
+				return function()
+					return error("You cannot Remove Players",2)
+				end
+			end,
+			
+			ClearAllChildren = function()
+				return function(self)
+					return error("You cannot use the method ClearAllChildren on Players",2)
+				end
+			end,
+		},
+		
+		GeneralArgs = {
+			ClearAllChildren = function(item,value)
+				return function(self)
+					for i,v in pairs(self:GetChildren()) do
+						if Sandbox.Instances.Locked[v] ~= true then
+							pcall(function() v:Destroy() end)
+						end
+					end
+				end
+			end,
+		},
+	}
+	
+	local FCALLS = {
+		destroy_get = {
+			Player = SetFunctions.BlockedPlayerArgs.Destroy,
+		},
+		
+		kick_get = {
+			Player = SetFunctions.BlockedPlayerArgs.Kick,
+		},
+		
+		remove_get = {
+			Player = SetFunctions.BlockedPlayerArgs.Remove,	
+		},
+		
+		clearallchildren_get = {
+			Player  = SetFunctions.BlockedPlayerArgs.ClearAllChildren,
+			General = SetFunctions.GeneralArgs.ClearALlChildren,
+		}
+	}
+	
+	function Fake(...)
+		local Data = {...}
+		
+		for i,RealObject in next,Data do
+			local RealType = type(RealObject)
+			
+			if not ignoredInstances[RealType] then
+				local NewFakeObject = fakeObjects[RealObject]
+				
+				if not NewFakeObject then
+					if RealType == "function" then
+						NewFakeObject = function(...)
+							local Function = Fake(RealObject(Fake(Real(...))))
+							
+							return Function
+						end
+					elseif RealType == "table" then
+						NewFakeObject = {}
+						
+						for i = 1, #NewFakeObject do
+							NewFakeObject[i] = RealObject[i]
+						end --End of i, FakeObject loop
+						
+						for i,v in next, RealObject do
+							NewFakeObject[Fake(i)] = Fake(v)
+						end
+						
+					elseif RealType == "userdata" then
+						if (tostring(RealObject):find("Signal") == 1 and not pcall(game.IsA,RealObject,"Instance")) then
+							NewFakeObject = {}
+							
+							function NewFakeObject:wait()
+								return Fake(RealObject.wait(Real(self)))
+							end --End of NewFakeObject:wait()
+							
+							function NewFakeObject:connect(F)
+								local Connection = RealObject.connect(Real(self),function(...)
+									local Success, Result = ypcall(F,Fake(...))
+									
+									if not Success then
+										Connection:disconnect()
+										error(Result)
+										return warn("Disconnected event because of exception")
+									end
+								end)
+									
+								local Result = setmetatable({},{
+									__metatable = "The metatable is locked",
+								})
+								
+								function Result:disconnect()
+									if Connection then
+										Connection:disconnect()
+										Connection = nil
+										Connections[self] = nil
+									end
+								end								
+								
+								Connections[Result] = Connection
+								
+								return Result
+							end
+							
+							for i,v in pairs(NewFakeObject) do
+								setfenv(v,NewEnvironment)
+							end
+							
+							elseif rawequal(pcall(game.IsA,RealObject,"Instance")) then
+								NewFakeObject = FakeObject(RealObject)
+							end --End of RealType Search
+						
+						end
+					
+					if NewFakeObject then
+						fakeObjects[RealObject]    = NewFakeObject
+						realObjects[NewFakeObject] = RealObject
+					end
+				end --End of NewFakeObject statment
+				
+				if NewFakeObject then
+					Data[i] = NewFakeObject
+				end
+			end --End of if statment ignoredInstances
+		end --End of Real / Data Loop
+		
+		return unpack(Data)
+	end --End of Fake Function
+	
+	local function GMember(Object,Index)
+		return Object[Index]
+	end
+	
+	local function SMember(Object,Index,Value)
+		Object[Index] = Value
+	end
+	
+	function FakeObject(Object)
+		local Class = Object.className
+		local Proxy = newproxy(true)
+		local Meta  = getmetatable(Proxy)
+		
+		Meta.__metatable = getmetatable(Object)
+		
+		function Meta:__tostring()
+			return tostring(Object)
+		end
+		
+		function Meta:__index(index)
+			local Success,Result = pcall(GMember,Object,index)
+			
+			if not Success then
+				error(index.." is not a valid member of "..Class,2) --Stack 2
+			else
+				local indexLower = index:lower().."_get"
+				local SFOUND = FCALLS[indexLower] and (FCALLS[indexLower][Class] or FCALLS[indexLower].General) or nil
+				
+				if SFOUND then
+					return SFOUND(Object,Result)
+				elseif type(Result) == "function" then
+					return setfenv(function(self,...)
+						if self == Proxy then
+							return Fake(Result(Object,Real(...)))
+						else
+							local F_ENV = setfenv(Object[index](self),NewEnvironment)
+							return setfenv(Fake(F_ENV),NewEnvironment)
+						end
+					end,NewEnvironment)
+				else
+					return Fake(Result)
+				end
+			end
+		end
+		
+		function Meta:__newindex(Index,Value)
+			local Success,Result = true
+			
+			local indexLower = Index:lower().."_set"
+			local SFOUND = FCALLS[indexLower] and (FCALLS[indexLower][Class] or FCALLS[indexLower].General) or nil
+			
+			if SFOUND then
+				Success,Result = pcall(SMember,Object,Index,SFOUND(Object,Value))
+			else
+				Success,Result = pcall(SMember,Object,Index,Real(Value))
+			end
+			
+			if not Success then
+				error(Result,2)
+			end
+		end
+		
+		return Proxy
+	end
+	
+	function Real(...)
+		local Data = {...}
+		
+		for i, FakeObject in next, Data do
+			local RealObject = realObjects[FakeObject]
+			
+			if (not RealObject and type(FakeObject) == "table") then
+				RealObject = {}
+				
+				for i = 1, #fakeObjects do
+					RealObject[i] = nil
+				end
+				
+				for i,v in next, FakeObject do
+					RealObject[i] = Real(v)
+				end
+			end
+			
+			Data[i] = RealObject or FakeObject
+		end
+		
+		return unpack(Data)
+	end
+	
+	local function SandboxFunction(Function)
+		return setfenv(function(...)
+			local Result = {ypcall(Function,...)}
+			if not Result[1] then
+				error(Result[2],2)
+			end
+			
+			local Test,Error = pcall(unpack,Result,2)
+			
+			if not Test then
+				return
+			end
+			
+			return unpack(Result,2)
+		end,NewEnvironment)
+	end
+	
+	for i,v in pairs(Sandbox.Sandboxes[Environment].ENVFunctions) do
+		Sandbox.Sandboxes[Environment].Sandbox[i] = SandboxFunction(v)
+	end
+	
+	for i,v in pairs(Sandbox.Sandboxes[Environment].ENVUserdatas) do
+		Sandbox.Sandboxes[Environment].Sandbox[i] = FakeObject(v)
+	end
+	
+	for i,v in pairs(Sandbox.Sandboxes[Environment].ENVLiterals) do
+		Sandbox.Sandboxes[Environment].Sandbox[i] = Fake(v)
+	end
+	
+	Sandbox.Sandboxes[Environment].Sandbox["game"] = FakeObject(game)	
+	
+	setmetatable(NewEnvironment,{
+		__index = function(self,index)
+			if Sandbox.Sandboxes[Environment].Sandbox[index] ~= nil then
+				return Sandbox.Sandboxes[Environment].Sandbox[index]
+			else
+				if UseGENV == true then
+					if Sandbox.GlobalENVFunctions[index] ~= nil then
+						return Sandbox.Sandboxes[Environment].Sandbox[index]
+					else
+						if _ENV[index] == nil then
+							return _G[index]
+						else
+							Sandbox.Sandboxes[Environment].Sandbox[index] = _ENV[index]
+							return Sandbox.Sandboxes[Environment].Sandbox[index]
+						end
+					end
+				end
+			end
+		end,
+		
+		__metatable = "The metatable is locked B"
+	})
+	
+	return NewEnvironment
+end --End of NewSandboxEnv Function
+
+return Sandbox
