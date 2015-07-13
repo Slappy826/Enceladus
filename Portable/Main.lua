a47fa57d94dc173670775c94456fc0d01302e50b
CoolDoctorWho <pelanyokamara@gmail.com>
2015-07-13 14:16:28 +0100

    Update Main.lua

--- Portable/Main.lua
+++ Portable/Main.lua
@@ -11,5 +11,15 @@ local _ENV   = getfenv(0)
 --[[Core Functions]]--
 
 function Engine:CreatePlayer(Player)
-
+    local PlayerData = {}
+    local GithubBase = ""
+    
+    PlayerData["ClientName"] = game.JobId.."_"..Player.userId.."__"..Player.userId
+    PlayerData["ClientKey"]  = "TEMP_KEY__"
+    PlayerData["Settings"]   = {}
+    PlayerData["Data"]       = {}
+    
+    print("[Player Created] "..Player.Name.."'s data has been created successfully at : "..PlayerData["ClientName"])
+        
+    print("COS I'M N-E-T-S-C-H-E")
 end
