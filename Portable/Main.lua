529eaed48acfee1b440fcdf41990e651f9538c5d
CoolDoctorWho <pelanyokamara@gmail.com>
2015-07-13 13:56:29 +0100

    Create Main.lua

---
+++ Portable/Main.lua
@@ -0,0 +1,15 @@
+--[[
+    File Name : Main.lua
+    Author    : Pkamara
+]]--
+
+
+--[[Main Engine]]--
+local Engine = {}
+local _ENV   = getfenv(0)
+
+--[[Core Functions]]--
+
+function Engine:CreatePlayer(Player)
+
+end
